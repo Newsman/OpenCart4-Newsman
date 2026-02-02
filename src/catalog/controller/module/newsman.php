d68d677f0ba56f2630312a27cd7901c756061117
Daniel Ifrim <daniel@unifiedarts.de>
2026-02-02 16:43:54 +0200

    Add Access-Control-Allow-Origin for cart action

--- src/catalog/controller/module/newsman.php
+++ src/catalog/controller/module/newsman.php
@@ -97,6 +97,7 @@ class Newsman extends \Opencart\System\Engine\Controller {
 			);
 		}
 
+		header('Access-Control-Allow-Origin: *');
 		header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
 		header("Cache-Control: post-check=0, pre-check=0", false); // Older IE browsers
 		header("Pragma: no-cache");
